66295a34e22af267ee2d2e93fdc917426ac4c7c6
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2021-06-11 13:22:47 -0300

    Exercicio com mutaions e state com vuex

---
+++ vuex/vuex-mutations/src/components/Aulas.vue
@@ -0,0 +1,45 @@
+<template>
+<div>
+  <h2>Aulas</h2>
+  <ul>
+    <li v-for="aula in aulas" :key="aula.nome">
+      <p>Nome da aula: {{aula.nome}} | Duração: {{aula.duracao}}</p>
+      <button @click="completarAula(aula)">Completar aula</button>
+    </li>
+  </ul>
+
+</div>
+</template>
+
+<script>
+export default {
+name:'Aulas',
+data(){
+  return{
+    aulas:[
+        {
+    nome: "HTML e CSS",
+    duracao: 15
+  },
+  {
+    nome: "JavaScript",
+    duracao: 30
+  },
+  {
+    nome: "UX Design",
+    duracao: 20
+  }
+    ]
+  }
+},
+methods:{
+  completarAula(aula){
+   this.$store.commit("COMPLETAR_AULA",aula)
+  }
+}
+}
+</script>
+
+<style>
+
+</style>
---
+++ vuex/vuex-mutations/src/store/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    aulasCompletas:[],
+  },
+  mutations: {
+    COMPLETAR_AULA(state,payload){
+     state.aulasCompletas.push(payload)
+    }
+  },
+})
