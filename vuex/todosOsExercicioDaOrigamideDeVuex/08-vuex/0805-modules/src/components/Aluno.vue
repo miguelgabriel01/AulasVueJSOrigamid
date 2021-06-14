9c521fb4954c36bac3f88da9f20d3f1965f12b91
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2021-06-14 10:59:11 -0300

    Envio das atvs restantes do curso de Vue da origamid

---
+++ vuex/todosOsExercicioDaOrigamideDeVuex/08-vuex/0805-modules/src/components/Aluno.vue
@@ -0,0 +1,16 @@
+<template>
+  <p>Aluno {{aulasCompletas}}</p>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  computed: {
+    ...mapState(["aulasCompletas"])
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ vuex/todosOsExercicioDaOrigamideDeVuex/08-vuex/0805-modules/src/store/acao.js
@@ -0,0 +1,21 @@
+export default {
+  namespaced: true,
+  state: {
+    empresa: 'apple',
+    acao: null,
+  },
+  mutations: {
+    UPDATE_ACAO(state, payload) {
+      state.acao = payload;
+    },
+  },
+  actions: {
+    puxarAcao(context) {
+      fetch('https://api.origamid.dev/stock/aapl/quote')
+        .then((r) => r.json())
+        .then((respostaJSON) => {
+          context.commit('UPDATE_ACAO', respostaJSON);
+        });
+    },
+  },
+};
